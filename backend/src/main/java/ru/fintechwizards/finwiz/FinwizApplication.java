94027ec00b480ee81d094b097ab95682afcc51f9
payl0adK <sanyarfx@yandex.ru>
2023-08-27 10:59:51 +0300

    backend init

---
+++ backend/src/main/java/ru/fintechwizards/finwiz/FinwizApplication.java
@@ -0,0 +1,13 @@
+package ru.fintechwizards.finwiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinwizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinwizApplication.class, args);
+	}
+
+}
